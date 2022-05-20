aeeba6ff6381e06a947caaf578f075f676b453de
Jeremy Faivre <contact@jeremyfa.com>
2022-05-20 10:59:49 +0200

    Add clay_allow_default_{touches/mouse_wheel/context_menu} defines to make it easier to export a web app that don't block scroll when inside an iframe

--- src/clay/web/WebConfig.hx
+++ src/clay/web/WebConfig.hx
@@ -3,16 +3,16 @@ package clay.web;
 @:structInit
 @:publicFields
 class WebConfig {
-    
+
     var windowId:String = 'app';
 
     var windowParent:js.html.Element = js.Browser.document.body;
 
-    var preventDefaultContextMenu:Bool = true;
+    var preventDefaultContextMenu:Bool = #if clay_allow_default_context_menu false #else true #end;
 
-    var preventDefaultMouseWheel:Bool = true;
+    var preventDefaultMouseWheel:Bool = #if clay_allow_default_mouse_wheel false #else true #end;
 
-    var preventDefaultTouches:Bool = true;
+    var preventDefaultTouches:Bool = #if clay_allow_default_touches false #else true #end;
 
     var preventDefaultKeys:Array<KeyCode> = [
         KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP, KeyCode.DOWN,
