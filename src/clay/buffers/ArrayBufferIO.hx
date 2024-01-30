9fe64e7500ce2c7ff6dfa96340b49b51c380a38c
Jeremy Faivre <contact@jeremyfa.com>
2024-01-30 21:54:12 +0100

    Fix Haxe 4.3 deprecation warnings

--- src/clay/buffers/ArrayBufferIO.hx
+++ src/clay/buffers/ArrayBufferIO.hx
@@ -13,35 +13,35 @@ class ArrayBufferIO {
 
     //8
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getInt8( buffer:ArrayBuffer, byteOffset:Int ) : Int {
 
             return untyped __global__.__hxcpp_memory_get_byte(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setInt8( buffer:ArrayBuffer, byteOffset:Int, value:Int ) : Void {
 
             untyped __global__.__hxcpp_memory_set_byte(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getUint8( buffer:ArrayBuffer, byteOffset:Int ) : UINT {
 
             return untyped __global__.__hxcpp_memory_get_byte(buffer, byteOffset) & 0xff;
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint8Clamped( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             setUint8( buffer, byteOffset, _clamp(value) );
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint8( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             untyped __global__.__hxcpp_memory_set_byte(buffer, byteOffset, value);
@@ -74,28 +74,28 @@ class ArrayBufferIO {
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getUint16( buffer:ArrayBuffer, byteOffset:Int ) : UINT {
 
             untyped return __global__.__hxcpp_memory_get_ui16(buffer, byteOffset) & 0xffff;
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getUint16_BE( buffer:ArrayBuffer, byteOffset:Int ) : UINT {
 
             untyped return __global__.__hxcpp_memory_get_ui16(buffer, byteOffset) & 0xffff;
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint16( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             untyped __global__.__hxcpp_memory_set_ui16(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint16_BE( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             untyped __global__.__hxcpp_memory_set_ui16(buffer, byteOffset, value);
@@ -104,55 +104,55 @@ class ArrayBufferIO {
 
     //32
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getInt32( buffer:ArrayBuffer, byteOffset:Int ) : Int {
 
             untyped return __global__.__hxcpp_memory_get_i32(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getInt32_BE( buffer:ArrayBuffer, byteOffset:Int ) : Int {
 
             untyped return __global__.__hxcpp_memory_get_i32(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setInt32( buffer:ArrayBuffer, byteOffset:Int, value:Int ) : Void {
 
             untyped __global__.__hxcpp_memory_set_i32(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setInt32_BE( buffer:ArrayBuffer, byteOffset:Int, value:Int ) : Void {
 
             untyped __global__.__hxcpp_memory_set_i32(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getUint32( buffer:ArrayBuffer, byteOffset:Int ) : UINT {
 
             untyped return __global__.__hxcpp_memory_get_ui32(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getUint32_BE( buffer:ArrayBuffer, byteOffset:Int ) : UINT {
 
             untyped return __global__.__hxcpp_memory_get_ui32(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint32( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             untyped __global__.__hxcpp_memory_set_ui32(buffer, byteOffset, value);
 
         }
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setUint32_BE( buffer:ArrayBuffer, byteOffset:Int, value:UINT ) : Void {
 
             untyped __global__.__hxcpp_memory_set_ui32(buffer, byteOffset, value);
@@ -161,56 +161,56 @@ class ArrayBufferIO {
 
     //Float
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getFloat32( buffer:ArrayBuffer, byteOffset:Int ) : Float {
 
             untyped return __global__.__hxcpp_memory_get_float(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getFloat32_BE( buffer:ArrayBuffer, byteOffset:Int ) : Float {
 
             untyped return __global__.__hxcpp_memory_get_float(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setFloat32( buffer:ArrayBuffer, byteOffset:Int, value:Float ) : Void {
 
             untyped __global__.__hxcpp_memory_set_float(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setFloat32_BE( buffer:ArrayBuffer, byteOffset:Int, value:Float ) : Void {
 
             untyped __global__.__hxcpp_memory_set_float(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getFloat64( buffer:ArrayBuffer, byteOffset:Int ) : Float {
 
             untyped return __global__.__hxcpp_memory_get_double(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function getFloat64_BE( buffer:ArrayBuffer, byteOffset:Int ) : Float {
 
             untyped return __global__.__hxcpp_memory_get_double(buffer, byteOffset);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setFloat64( buffer:ArrayBuffer, byteOffset:Int, value:Float ) : Void {
 
             untyped __global__.__hxcpp_memory_set_double(buffer, byteOffset, value);
 
         }
 
-        #if !clay_no_inline_buffers @:extern inline #end
+        #if !clay_no_inline_buffers extern inline #end
         public static function setFloat64_BE( buffer:ArrayBuffer, byteOffset:Int, value:Float ) : Void {
 
             untyped __global__.__hxcpp_memory_set_double(buffer, byteOffset, value);
@@ -219,7 +219,7 @@ class ArrayBufferIO {
 
 //Internal
 
-    #if !clay_no_inline_buffers @:extern inline #end
+    #if !clay_no_inline_buffers extern inline #end
     //clamp a Int to a 0-255 Uint8 (for Uint8Clamped array)
     static function _clamp(_in:Float) : Int {
 
