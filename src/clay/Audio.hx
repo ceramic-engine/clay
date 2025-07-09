a2e03fc6aa38e3e267ad4d7f28230c6ae02c8337
Jeremy Faivre <contact@jeremyfa.com>
2025-07-10 00:13:38 +0200

    Drop support of OpenAL (in favor of Soloud)

--- src/clay/Audio.hx
+++ src/clay/Audio.hx
@@ -2,8 +2,6 @@ package clay;
 
 #if clay_web
 typedef Audio = clay.web.WebAudio;
-#elseif (clay_native && clay_openal)
-typedef Audio = clay.openal.OpenALAudio;
 #elseif (clay_native && clay_soloud)
 typedef Audio = clay.soloud.SoloudAudio;
 #end
--- src/clay/openal/AL.hx
+++
@@ -1,9 +0,0 @@
-package clay.openal;
-
-typedef Context     = openal.AL.Context;
-typedef Device      = openal.AL.Device;
-typedef AL          = openal.AL;
-typedef ALuint      = openal.AL.ALuint;
-typedef ALError     = openal.AL.ALError;
-typedef ALC         = openal.AL.ALC;
-typedef ALCError    = openal.AL.ALCError;
